1224081e7ea26cdc807815d96b6d570bafc7644d
Utso-Majumder <57312923+Utso-Majumder@users.noreply.github.com>
2020-06-26 22:28:05 +0530

    updated rockPaperScissors

--- rockPaperScissor.py
+++ rockPaperScissor.py
@@ -4,33 +4,44 @@ from string import *
 moves = ["ROCK","PAPER","SCISSORS"]
 val = [1,2,3]
 
-def rpsFunc(pTurn):
+def rpsFunc(pTurn,s1,s2):
     v = choice(moves)
     pTurn = pTurn.upper()
     if pTurn != "ROCK" and pTurn != "SCISSORS" and pTurn != "PAPER":
         print("***Invalid Input!! Please retry***")
-        return
+        return s1,s2
     print("Computer chooses : ", end="")
     print(v)
     d = dict(zip(moves,val))
     if v==pTurn:
         print("Both play same move. Tied.")
-        return
+        return s1,s2
 
     if v=="ROCK" and pTurn=="SCISSORS":
         print("Computer wins !")
+        s2+=1
+        return s1,s2
     elif pTurn=="ROCK" and v=="SCISSORS":
         print("Player wins !")
+        s1+=1
+        return s1,s2
     else:
         if d.get(v) > d.get(pTurn):
             print("Computer wins !")
+            s2+=1
+            return s1,s2
         elif d.get(v) < d.get(pTurn):
             print("Player wins !")
+            s1+=1
+            return s1,s2
 
 pT = input("Your turn, player. Choose from Rock, Paper, Scissors: ")
-n=2
-for i in range(n):
-    rpsFunc(pT)
+sComp=0
+sPlayer=0
+n=1
+while n>0:
+    sPlayer, sComp = rpsFunc(pT, sPlayer, sComp)
+    print("Computer "+str(sComp)+" : "+str(sPlayer)+" Player ")
     r = input("Continue? (Y/N):")
     r = r.upper()
     if r=="Y":
