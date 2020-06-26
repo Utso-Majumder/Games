a5c09ca00a57d17a4f00e9333956156758bf56bc
Utso-Majumder <57312923+Utso-Majumder@users.noreply.github.com>
2020-06-26 16:44:19 +0530

    updated

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,10 +1,18 @@
 from random import *
 
-n = int(input("Enter a maximum range : "))
-m = int(input("Enter a minimum range : "))
-v = randint(m,n)
-h = int(input("Guess the number : "))
-if h==v:
-    print("You've guessed correctly!")
-else:
-    print("You've guessed wrongly!")
+lim = 2
+for i in range(lim):
+    n = int(input("Enter a maximum range : "))
+    m = int(input("Enter a minimum range : "))
+    v = randint(m, n)
+    h = int(input("Guess the number : "))
+    if h == v:
+        print("You've guessed correctly!")
+    else:
+        print("You've guessed wrongly!")
+    r = input("Continue? (Y/N):")
+    r = r.upper()
+    if r == "Y":
+        n += 1
+    else:
+        break
